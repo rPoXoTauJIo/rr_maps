a176dc8747c69285df84728563e57e4e8cd95389
rpoxo <irpoxotaujio@gmail.com>
2017-02-12 22:32:21 +0300

    added some clear stuff for integration test

--- rr_init.py
+++ rr_init.py
@@ -4,7 +4,8 @@ import rr_controller
 
 def init():
     debugger = rr_debugger.Debugger()
-    debugger.debugMessage('INIT: Server initialized', ['echo', 'udp']) # mandatory string for integration testing
+    # for integration testing
+    debugger.debugMessage('INIT: Server initialized', ['echo', 'udp'])
     controller = rr_controller.MapsController()
     #maplist = controller.get_current_maplist_engine()
     # for entry in maplist:
